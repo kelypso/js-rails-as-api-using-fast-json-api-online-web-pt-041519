2aafd2ec365cf3cca49b9a3e1ee4f36c415a6777
github username <github email address>
2019-11-17 21:57:30 -0700

    Done.

--- app/serializers/sighting_serializer.rb
+++ app/serializers/sighting_serializer.rb
@@ -1,4 +1,8 @@
 class SightingSerializer
   include FastJsonapi::ObjectSerializer
-  attributes 
+  attributes :created_at#, :bird, :location 
+  # Access normal attrs as well as related objs
+  # Cannot control which related obj attrs are included, all will render
+  belongs_to :bird 
+  belongs_to :location
 end
